a10bbec667e8570fdeb8ea687a74aee1a7d3caf8
LuaBerry <lazpberry1012@gmail.com>
2023-11-27 18:46:34 +0900

    Fix heroku bug

--- src/init.js
+++ src/init.js
@@ -1,7 +1,8 @@
 import app from "./app";
 
+const PORT = process.env.PORT || 9000;
 
 const handleListening = () =>
     console.log(`Server listening on port http://localhost:${PORT}`);
 
-app.listen(process.env.PORT || 9000, handleListening);
+app.listen(PORT, handleListening);
